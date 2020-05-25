0f756bc9211ef09e02c7bbe5cd637f513e293e4c
Achraf RRAMI <onlyjs@tuta.io>
2020-05-25 22:09:35 +0000

    add qbittorrent

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo snap install spotify
 
 # VIM
 sudo apt-get update
-sudo apt-get install vim
+sudo apt-get install -y vim
 
 # INSOMNIA
 sudo snap install insomnia
@@ -21,7 +21,7 @@ sudo snap install heroku
 
 # GIT
 sudo apt-get update
-sudo apt install git
+sudo apt install -y git
 
 # GITHUB CLI
 wget https://github.com/cli/cli/releases/download/v0.8.0/gh_0.8.0_linux_386.deb
@@ -30,19 +30,24 @@ rm gh_0.8.0_linux_386.deb
 
 # NODE & NPM
 sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 
 # DENO
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
 # MONGODB COMPASS
 sudo apt-get update
-sudo apt-get install mongodb-compass
+sudo apt-get install -y mongodb-compass
 
 # YARN
 npm i yarn --global
 
 # SQLITE BROWSER
 sudo apt-get update
-sudo apt-get install sqlitebrowser
+sudo apt-get install -y sqlitebrowser
+
+# QBITTORENT
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo apt-get update
+sudo apt-get install -y qbittorrent
